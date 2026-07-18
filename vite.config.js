ec01152d449986f1177016d5ccdef84d21327df3
mendrika <mendrikavamet@gmail.com>
2026-07-18 12:27:48 +0300

    Fix 404 error on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // AJOUTE CETTE LIGNE :
+  // Remplace 'valisoamendrika.github.io' par le nom de ton dépôt si nécessaire
+  base: '/valisoamendrika.github.io/', 
 })
